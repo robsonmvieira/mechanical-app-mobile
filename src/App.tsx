31ca90880fe90f4efd12658c6d3032b8dbfc4605
Robson Maia <robsonmvieira@gmail.com>
2020-08-12 16:19:46 -0300

    feat: add splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
-// import { Container } from './styles';
-
-const App: React.FC = () => {
-  return <View />;
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return (
+    <>
+      <View style={styles.container}>
+        <Text>Tela inicial</Text>
+        <Text>Será que vai dar certo</Text>
+      </View>
+    </>
+  );
 };
 
 export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#333',
+  },
+});
