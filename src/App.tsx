133315553c0874c90b6233edf3bf08b0f845ce7f
Robson Maia <robsonmvieira@gmail.com>
2020-08-14 09:20:05 -0300

    feat: put navigationContainer arround App Screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import React, {useEffect} from 'react';
 import 'react-native-gesture-handler';
 import SplashScreen from 'react-native-splash-screen';
 import Routes from './routes';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
   return (
-    <>
+    <NavigationContainer>
       <Routes />
-    </>
+    </NavigationContainer>
   );
 };
 
