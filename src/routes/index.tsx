a503ed7fc3f53e147ac42d9efec483aeb4d4bc85
Robson Maia <robsonmvieira@gmail.com>
2020-08-20 21:51:09 -0300

    feat: add new router to app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,15 @@ import * as React from 'react';
 
 import {createStackNavigator} from '@react-navigation/stack';
 import Login from '../pages/Login';
+import SignUp from '../pages/SignUp';
 
 const Stack = createStackNavigator();
 
 function Routes() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Cadastrar">
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Cadastrar" component={SignUp} />
     </Stack.Navigator>
   );
 }
